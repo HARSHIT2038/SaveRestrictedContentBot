df17c0929d8b4ef0149cc8ff3adadbeb38e86268
HARSHIT BEN <pauktoe200356@gmail.com>
2023-04-07 23:38:22 +0630

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("2666511", default=None, cast=int)
-API_HASH = config("5135858a03edc1f0bac8a6dc8556f08d", default=None)
-BOT_TOKEN = config("5878216369:AAF9VR588GcVoKCcFHum8K_fxkRegg7bwFU", default=None)
-SESSION = config("BQALIYkXxiKQvhq9wgCUCkqMuZlAWrZ0Z2FfC9ZaIDeYyvNXKTiyQax7etFHnasL6XS2iV4spqQjT8xYYFH3-_oe0gJPuVW-OmR1j9jbuGJ_LIYZj3IgfalZkjHc2JfXRKOscPj3JmkcFSnJzszngqqn0-tdT0vAA2DWiiqYj076nUYfdxLE5l_sRtx0AMPcdEjJFSgbH1WSDKJlfxeMS4wd_RZrkEbkyaaoy4uyDy5B7R8sswFY_DpWJvFXMWTaF-ATdLXBpcDuaNrECbDamhdAcRq_QsVg2vw1fRY59QxKJuiT6hlP34Z-DJTMwVdEKnU4y-imPyP7ZYaRvMaXkQWUW7B1tQA", default=None)
-FORCESUB = config("saveown_bot", default=None)
-AUTH = config("1538291125", default=None, cast=int)
+API_ID = 2666511
+API_HASH = "5135858a03edc1f0bac8a6dc8556f08d"
+BOT_TOKEN = "5878216369:AAF9VR588GcVoKCcFHum8K_fxkRegg7bwFU"
+SESSION = "BQALIYkXxiKQvhq9wgCUCkqMuZlAWrZ0Z2FfC9ZaIDeYyvNXKTiyQax7etFHnasL6XS2iV4spqQjT8xYYFH3-_oe0gJPuVW-OmR1j9jbuGJ_LIYZj3IgfalZkjHc2JfXRKOscPj3JmkcFSnJzszngqqn0-tdT0vAA2DWiiqYj076nUYfdxLE5l_sRtx0AMPcdEjJFSgbH1WSDKJlfxeMS4wd_RZrkEbkyaaoy4uyDy5B7R8sswFY_DpWJvFXMWTaF-ATdLXBpcDuaNrECbDamhdAcRq_QsVg2vw1fRY59QxKJuiT6hlP34Z-DJTMwVdEKnU4y-imPyP7ZYaRvMaXkQWUW7B1tQA"
+FORCESUB = "saveown_bot"
+AUTH = 1538291125
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
